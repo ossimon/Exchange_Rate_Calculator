0b38a21d373634a9bbf93a0371689e639d84cfa8
Oskar Simon <oskarsimon3@gmail.com>
2023-02-19 15:49:26 +0100

    Completed the getExchangeRate(currency) function. Now the app correctly calculates the output amount of money.

--- script.js
+++ script.js
@@ -1,25 +1,44 @@
-function calculateOutput(inputValue) {
-    const numberValue = Number(inputValue)
-    if (isNaN(numberValue)) {
-        return 'Your input should be a number!';
-    }
+async function getExchangeRate(currency) {
+    let response = await fetch('http://api.nbp.pl/api/exchangerates/rates/a/' + currency + '/?format=json')
+    let data = await response.json()
+    let exchangeRate = data.rates[0].mid
     
-    const outputValue = numberValue * 2;
-    return outputValue.toString();
+    return exchangeRate
 }
 
-function updateBox(boxId) {
-    const inputValue = document.getElementById(boxId).value;
-    const outputValue = calculateOutput(inputValue);
+async function calculateOutput(inputValue, boxId) {
+    const numberValue = Number(inputValue)
+    if (isNaN(numberValue)) {
+        return 'Your input should be a number!'
+    }
 
-    var boxToUpdateId;
+    const exchangeRate = await getExchangeRate("gbp")
+    let outputValue
     switch (boxId) {
         case 'send':
-            boxToUpdateId = 'receive';
-            break;
+            outputValue = numberValue * exchangeRate
+            break
         case 'receive':
-            boxToUpdateId = 'send';
-            break;
+            outputValue = numberValue / exchangeRate
+            break
     }
-    document.getElementById(boxToUpdateId).value = outputValue;
+    outputValue = Math.floor(outputValue * 100) / 100
+
+    return outputValue.toString()
+}
+
+async function updateBox(boxId) {
+    const inputValue = document.getElementById(boxId).value
+    const outputValue = await calculateOutput(inputValue, boxId)
+
+    let boxToUpdateId
+    switch (boxId) {
+        case 'send':
+            boxToUpdateId = 'receive'
+            break
+        case 'receive':
+            boxToUpdateId = 'send'
+            break
+    }
+    document.getElementById(boxToUpdateId).value = outputValue
 }
